60c033fe3f38a01a2a2a8aec91b2a7f4cde1cb65
Andrew Ross <andrew@vermonster.com>
2016-02-02 00:34:54 -0500

    get melting temperature test passing / consistent with OligoCalc

--- guide/constants.py
+++ guide/constants.py
@@ -1,7 +1,50 @@
 nucleotide_masses = { 'A': 313.21, 'G': 329.21, 'C': 289.18, 'T': 304.2 }
 
 # grabbed from http://biotools.nubic.northwestern.edu/OligoCalc.html
-neighbor_entropies = { 'AA': 21.9, 'AT': 15.2, 'TA': 18.4, 'CA': 21.0, 'GT': 25.5,
-                       'CT': 16.4, 'GA': 23.5, 'CG': 29.0, 'GC': 26.4, 'GG': 28.4 }
-neighbor_enthalpies = { 'AA': 8.0, 'AT':  5.6, 'TA':  6.6, 'CA':  8.2, 'GT': 9.4,
-                        'CT': 6.6, 'GA':  8.8, 'CG': 11.8, 'GC': 10.5, 'GG': 10.9 }
+# original source is http://www.ncbi.nlm.nih.gov/pmc/articles/PMC146261/pdf/244501.pdf
+
+neighbor_entropies = { # cal/mol/K
+'AA': 21.9,
+'AC': 25.5,
+'AG': 16.4,
+'AT': 15.2,
+
+'CA': 21.0,
+'CC': 28.4,
+'CG': 29.0,
+'CT': 16.4,
+
+'TA': 18.4,
+'TC': 23.5,
+'TG': 21.0,
+'TT': 21.9,
+
+'GA': 23.5,
+'GC': 26.4,
+'GG': 28.4,
+'GT': 25.5
+}
+
+neighbor_enthalpies = { # cal/mol
+'AA': 8000,
+'AC': 9400,
+'AG': 6600,
+'AT': 5600,
+
+'CA': 8200,
+'CC': 10900,
+'CG': 11800,
+'CT': 6600,
+
+'GA': 8800,
+'GC': 10500,
+'GG': 10900,
+'GT': 9400,
+
+'TA': 6600,
+'TC': 8800,
+'TG': 8200,
+'TT': 8000
+}
+
+ideal_gas_constant = 1.987 # cal/(mol*K)
--- guide/spacer_sequence.py
+++ guide/spacer_sequence.py
@@ -1,4 +1,4 @@
-from .constants import nucleotide_masses, neighbor_entropies, neighbor_enthalpies
+from .constants import nucleotide_masses, neighbor_entropies, neighbor_enthalpies, ideal_gas_constant
 from .helpers import neighbors
 from collections import Counter
 import math
@@ -13,23 +13,36 @@ class SpacerSequence():
     def hairpin_score(self):
         return None
 
+
+    def melting_entropy_change(self):
+        return sum(entropy * self.neighbor_counts[nn]
+                for nn, entropy in neighbor_entropies.items())
+
+    def melting_enthalpy_change(self):
+        return sum(enthalpy * self.neighbor_counts[nn]
+                for nn, enthalpy in neighbor_enthalpies.items())
+
     def melting_temperature(self):
         # using nearest-neighbor, following
         # http://biotools.nubic.northwestern.edu/OligoCalc.html
         assert(len(self.sequence) >= 8)
         assert(len(self.sequence) <= 20)
         assert(self.gc_content() > 0)
-        # additional assumptions:
-        # This apparently assumes that:
-        # - len(sequence) >= 8
-        # - nucleotide_counts
-        dS = sum(entropy * self.neighbor_counts[nn]
-                for nn, entropy in neighbor_entropies.items())
-        dH = sum(enthalpy * self.neighbor_counts[nn]
-                for nn, enthalpy in neighbor_enthalpies.items())
-        primer_concentration = 50. * 1e-9 # moles
-        sodium_concentration = 50. * 1e-3 # moles
-        return (dH-3.4)/(dS+1.987*math.log(1./sodium_concentration)) - 7.21*math.log(sodium_concentration)
+
+        dS = self.melting_entropy_change()
+        dH = self.melting_enthalpy_change()
+        primer_concentration = 50. * 1e-9 # mol
+        helix_initiation_free_energy_change = 3400 # cal/mol
+        self_complementary_free_energy_change = 0 # for now, should be 400 sometimes
+
+        # this is constant and could be ignored, but i want the numbers to match
+        sodium_concentration = 50. * 1e-3 # mol
+        sodium_tm_adjustment = 7.21 * math.log(sodium_concentration)
+
+        base_tm_calculation = (dH - helix_initiation_free_energy_change) \
+            / (dS + ideal_gas_constant * math.log(1./primer_concentration))
+
+        return base_tm_calculation + sodium_tm_adjustment - 272.9
 
     def molecular_mass(self):
         # not counting extra phosphates, because it's constant
