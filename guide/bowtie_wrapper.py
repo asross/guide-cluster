74ec897c83a7d600cb73a55ef206042e4914a988
Andrew Ross <andrew@vermonster.com>
2016-02-19 20:12:42 -0500

    speed up bowtie wrapping and handle edge cases

--- guide/bowtie_wrapper.py
+++ guide/bowtie_wrapper.py
@@ -1,21 +1,25 @@
 from .helpers import output_of
 from .bowtie_result import *
 
-# human genome index
-bowtie_index = 'GCA_000001405.15_GRCh38_no_alt_analysis_set'
-
 class BowtieWrapper():
-    def __init__(self, dna_sequence):
+    def __init__(self, dna_sequence, n_mismatches=2, bowtie_index='GCA_000001405.15_GRCh38_no_alt_analysis_set'):
         self.dna_sequence = dna_sequence
+        self.n_mismatches = n_mismatches
+        self.bowtie_index = bowtie_index
 
     def search_command(self):
-        return 'cd `which bowtie | xargs dirname` && bowtie -v 3 -a --best {0} -c {1} --quiet'.format(bowtie_index, self.dna_sequence)
+        return 'cd `which bowtie | xargs dirname` && bowtie -v {2} --mm -a --best {0} -c {1} --quiet'.format(self.bowtie_index, self.dna_sequence, self.n_mismatches)
 
     def parse(self, bowtie_output):
-        return [self.parse_line(line) for line in bowtie_output.strip().split("\n")]
+        if bowtie_output:
+            return [self.parse_line(line) for line in bowtie_output.strip().split("\n")]
+        else:
+            return []
 
     def parse_line(self, line):
-        _i, direction, chromosome, index, _seq, _quals, multiplicity, mismatches = line.split("\t")
+        values = line.split("\t")
+        if len(values) == 7: values.append('')
+        _i, direction, chromosome, index, _seq, _quals, multiplicity, mismatches = values
         mismatches = mismatches.split(',') if mismatches else []
         is_reversed = (direction == '-')
         return [is_reversed, chromosome, int(index), int(multiplicity), mismatches]
--- tests/test_bowtie_wrapper.py
+++ tests/test_bowtie_wrapper.py
@@ -5,7 +5,7 @@ from guide.bowtie_result import *
 
 class TestBowtieWrapper(unittest.TestCase):
     def test_search(self):
-        search = BowtieWrapper('TCTCAGCGCGCTTTTCACCG').search()
+        search = BowtieWrapper('TCTCAGCGCGCTTTTCACCG', n_mismatches=3).search()
         self.assertEqual(len(search.matches()), 20)
         match = search.exact_match()
         self.assertEqual('chr8', match.chromosome)
@@ -16,6 +16,10 @@ class TestBowtieWrapper(unittest.TestCase):
         search1 = BowtieWrapper('TCTCAGCGCGCTTTTCACCG').search()
         search2 = BowtieResult.from_json(search1.to_json())
         self.assertEqual(search1._matches, search2._matches)
+
+    def test_empty_search(self):
+        search = BowtieWrapper('TCTCAGCGCGCTTTTCACCGTTTTTTTTT').search()
+        self.assertEqual(len(search.matches()), 0)
 
 if __name__ == '__main__':
     unittest.main()
